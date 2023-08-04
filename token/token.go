26d45fb6d48c912782728319de14bd34475506ed
Justin Jones <hock3ydud3@gmail.com>
2023-08-03 21:55:53 -0500

    initial working lexer

---
+++ token/token.go
@@ -0,0 +1,49 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	// check for kw
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	// if not kw, then it's an ident
+	return IDENT
+}
